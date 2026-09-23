8a702569f8016729d0c72119bb6ef88729454b96
IceCrowNT <namdh0912@gmail.com>
2026-09-23 08:56:16 +0700

    add icon tab trình duyệt

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,11 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Tra cứu phí căn hộ An Đồng",
-  description: "Tra cứu trạng thái đóng phí căn hộ và quản trị dữ liệu thu phí nội bộ."
+  description: "Tra cứu trạng thái đóng phí căn hộ và quản trị dữ liệu thu phí nội bộ.",
+  icons: {
+    icon: "/images/logo-hoanghuy.jpg",
+    apple: "/images/logo-hoanghuy.jpg",
+  }
 };
 
 export default function RootLayout({
